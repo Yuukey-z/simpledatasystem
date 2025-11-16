a5c95e7f8da1bcc74475d0561304b7c80c85ffa1
Yuukey-z <rizkymaouu@gmail.com>
2025-11-16 11:17:50 +0800

    added some feature : - added merkBarang attribute - added beratBarang attribute
    and some changes on the code due to adding attribute.

--- src/Barang.java
+++ src/Barang.java
@@ -2,20 +2,26 @@ package data;
 
 public class Barang {
     // Atribut (field)
-    private String nama;
-    private double harga;
-    private int stok;
+    private String namaBarang;
+    private double hargaBarang;
+    private int stokBarang;
+    private String merkBarang;
+    private double beratBarang;
     // Constructor untuk memberi nilai awal
-    public Barang(String nama, double harga, int stok) {
-        this.nama = nama;
-        this.harga = harga;
-        this.stok = stok;
+    public Barang(String namaBarang, double hargaBarang, int stokBarang, String merkBarang, double beratBarang) {
+        this.namaBarang = namaBarang;
+        this.hargaBarang = hargaBarang;
+        this.stokBarang = stokBarang;
+        this.merkBarang = merkBarang;
+        this.beratBarang = beratBarang;
     }
     // Method untuk menampilkan data barang
     public void tampilkanData() {
-        System.out.println("Nama Barang : " + nama);
-        System.out.println("Harga       : Rp" + harga);
-        System.out.println("Stok        : " + stok);
+        System.out.println("Nama Barang : " + namaBarang);
+        System.out.println("Harga       : Rp" + hargaBarang);
+        System.out.println("Stok        : " + stokBarang);
+        System.out.println("Merek Barang: " + merkBarang);
+        System.out.println("Berat       : " + beratBarang +"g");
         System.out.println("-----------------------------");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import data.Barang;
 public class Main {
     public static void main(String[] args) {
         // Membuat beberapa objek Barang
-        Barang b1 = new Barang("Router WiFi", 350000, 5);
-        Barang b2 = new Barang("Switch 8 Port", 250000, 3);
-        Barang b3 = new Barang("Kabel LAN 10m", 50000, 10);;
+        Barang b1 = new Barang("Router WiFi", 350000, 5, "ROBOT", 1000);
+        Barang b2 = new Barang("Switch 8 Port", 250000, 3, "CommScope", 120);
+        Barang b3 = new Barang("Kabel LAN 10m", 50000, 10, "Vention", 1245);
 
         // Menampilkan data semua barang
         System.out.println("=== DAFTAR BARANG TOKO KOMPUTER ===");
